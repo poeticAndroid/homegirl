84d1df2fcb01b4d5fb3f39a99ece47684b4d391c
poetic android <i@poeticandroid.online>
2019-06-04 17:46:33 +0200

    better demonstration of gamebtn

--- examples/game_input.lua
+++ examples/game_input.lua
@@ -1,39 +1,62 @@
 dofile("./examples/screendrag.lua")
 
-scrn = createscreen(0, 3)
+font = loadfont("./examples/fonts/Victoria.8b.gif")
+scrn = createscreen(0, 2)
 bgcolor(0)
-setcolor(1, 15, 15, 15)
+copymode(2)
 
-px = 160
-py = 90
+players = {
+  {
+    x = 160,
+    y = 90
+  },
+  {
+    x = 80,
+    y = 90
+  },
+  {
+    x = 240,
+    y = 90
+  }
+}
 
 function _step()
   cls()
-  btn = gamebtn()
-  if btn & 1 > 0 then
-    px = px + 1
+  for pn = 0, 2, 1 do
+    p = players[1 + pn]
+    btn = gamebtn(pn)
+    if btn & 1 > 0 then
+      p.x = p.x + 1
+    end
+    if btn & 2 > 0 then
+      p.x = p.x - 1
+    end
+    if btn & 4 > 0 then
+      p.y = p.y - 1
+    end
+    if btn & 8 > 0 then
+      p.y = p.y + 1
+    end
+    fgcolor(1 + pn)
+    text(pn, font, p.x + 4, p.y - 8)
+    bar(p.x, p.y, 16, 16)
+    fgcolor(0)
+    if btn & 16 > 0 then
+      setcolor(1 + pn, 0, 15, 0)
+      text("A", font, p.x, p.y)
+    end
+    if btn & 32 > 0 then
+      setcolor(1 + pn, 15, 0, 0)
+      text("B", font, p.x + 8, p.y)
+    end
+    if btn & 64 > 0 then
+      setcolor(1 + pn, 0, 0, 15)
+      text("X", font, p.x, p.y + 8)
+    end
+    if btn & 128 > 0 then
+      setcolor(1 + pn, 15, 15, 0)
+      text("Y", font, p.x + 8, p.y + 8)
+    end
   end
-  if btn & 2 > 0 then
-    px = px - 1
-  end
-  if btn & 4 > 0 then
-    py = py - 1
-  end
-  if btn & 8 > 0 then
-    py = py + 1
-  end
-  if btn & 16 > 0 then
-    setcolor(1, 0, 15, 0)
-  end
-  if btn & 32 > 0 then
-    setcolor(1, 15, 0, 0)
-  end
-  if btn & 64 > 0 then
-    setcolor(1, 0, 0, 15)
-  end
-  if btn & 128 > 0 then
-    setcolor(1, 15, 15, 0)
-  end
-  bar(px, py, 16, 16)
   dragscreen(scrn)
 end
