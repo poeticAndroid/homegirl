f21bf63af25de23037836b86129a18a3cd6016a9
poetic android <i@poeticandroid.online>
2019-09-14 02:39:18 +0200

    script for installing dependencies on Ubuntu

---
+++ install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "This script will install dependencies for Homegirl on Ubuntu based Linux distros."
+echo "Press [Enter] to continue or [Ctrl+C] to cancel."
+read enter
+
+sudo apt-get install libsdl2-2.0-0 libfreeimage3 liblua5.3-dev libcurl3-gnutls 
