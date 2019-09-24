a1acc20f024223e5a168aad8002058025c3bce28
poetic android <i@poeticandroid.online>
2019-09-24 16:08:15 +0200

    don't delete `install_dependencies` until it's run

--- homegirl.sh
+++ homegirl.sh
@@ -2,14 +2,7 @@
 cd "$(dirname "$0")"
 
 if [ "$(uname)" == "Darwin" ]; then
-  ./homegirl_macos && rm ./install_dependencies.sh
+  ./homegirl_macos || open -a Terminal ./install_dependencies.sh
 else
-  ./homegirl_linux && rm ./install_dependencies.sh
-fi
-if [ -f ./install_dependencies.sh ]; then
-  if [ "$(uname)" == "Darwin" ]; then
-    open -a Terminal ./install_dependencies.sh
-  else
-    x-terminal-emulator -e ./install_dependencies.sh
-  fi
+  ./homegirl_linux || x-terminal-emulator -e ./install_dependencies.sh
 fi
