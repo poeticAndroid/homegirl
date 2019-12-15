81985b35a8294f87fd234e5c1728108c70dd9074
poetic android <i@poeticandroid.online>
2019-12-15 19:30:13 +0100

    bump

--- source/machine.d
+++ source/machine.d
@@ -26,7 +26,7 @@ import pixmap;
 import image_loader;
 import network;
 
-const VERSION = "0.9.17"; /// version of the software
+const VERSION = "0.10.0"; /// version of the software
 
 /**
   Class representing "the machine"!
